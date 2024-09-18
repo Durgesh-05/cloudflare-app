008d52a73bc0c3c2d5623b3fdbd7332991133fdc
Durgesh-05 <durgeshkumar.d@somaiya.edu>
2024-09-18 16:26:23 +0530

    Added routing in serverless

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+		console.log(request.method);
+		console.log(request.url);
+		console.log(request.headers);
+
+		// Based on url and methods we have to do routing
+		if (request.method === 'POST' && request.url === '/api/v1/user/signup') {
+			return Response.json({ message: 'You are on signup page' });
+		}
+
 		return new Response('Hello World!');
 	},
 } satisfies ExportedHandler<Env>;
+
+// We can deploy worker directly using wrangler
+// first login your cloudflare account - npx wrangler login
+// Then deploy this is a very minimalistic application since routing and other things are very bad in this we have to use request object everytime and see url request so we need library to work along with it
+// To deploy - npm run deploy
