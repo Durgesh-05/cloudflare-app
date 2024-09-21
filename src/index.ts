e58f1c257d188f1954a50d96a23c3fd59b2dd113
Durgesh-05 <durgeshkumar.d@somaiya.edu>
2024-09-21 22:48:13 +0530

    Added connection to db

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,50 @@
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
+// To use prisma in cloudflare worker we have to use connection pool from databases and remove dependency that is related to nodejs because we know that cloudflare workers created their own runtime thats why express wont work on it so to use prisma for orm we need to do this
+
+// We will use prisma accelerate for connection pooling and generate the client using the flag --no-engine to work in serverless
+
+interface Env {
+	DATABASE_URL: string;
+}
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		console.log(request.method);
-		console.log(request.url);
-		console.log(request.headers);
+	async fetch(request, env: Env, ctx): Promise<Response> {
+		// console.log(request.method);
+		// console.log(request.url);
+		// console.log(request.headers);
 
-		// Based on url and methods we have to do routing
-		if (request.method === 'POST' && request.url === '/api/v1/user/signup') {
-			return Response.json({ message: 'You are on signup page' });
-		}
+		// // Based on url and methods we have to do routing
+		// if (request.method === 'POST' && request.url === '/api/v1/user/signup') {
+		// 	return Response.json({ message: 'You are on signup page' });
+		// }
 
-		return new Response('Hello World!');
+		// return new Response('Hello World!');
+
+		const prisma = new PrismaClient({
+			datasourceUrl: env.DATABASE_URL,
+		}).$extends(withAccelerate());
+
+		await prisma.log.create({
+			data: {
+				level: 'Info',
+				message: `${request.method} ${request.url}`,
+				meta: {
+					headers: JSON.stringify(request.headers),
+				},
+			},
+		});
+
+		const { data, info } = await prisma.log
+			.findMany({
+				take: 20,
+				orderBy: {
+					id: 'desc',
+				},
+			})
+			.withAccelerateInfo();
+		console.log(JSON.stringify(info));
+		return Response.json({ logs: data });
 	},
 } satisfies ExportedHandler<Env>;
 
